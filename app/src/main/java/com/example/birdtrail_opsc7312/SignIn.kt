2a9e49a63f116f2f33b15f87ec23e54dfd6c3a75
jake1970 <jakeyjyoung@gmail.com>
2023-09-22 09:32:40 +0200

    Landing Page Design Progress

--- app/src/main/java/com/example/birdtrail_opsc7312/SignIn.kt
+++ app/src/main/java/com/example/birdtrail_opsc7312/SignIn.kt
@@ -7,5 +7,7 @@ class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in)
+
+
     }
 }
