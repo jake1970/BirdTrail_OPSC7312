9fa19141b024b4ea4d773e67409a4985fd2518a0
CopperPaint <jsaul1250@gmail.com>
2023-10-06 12:48:27 +0200

    bottom navigation

--- app/src/main/java/com/example/birdtrail_opsc7312/Homepage.kt
+++ app/src/main/java/com/example/birdtrail_opsc7312/Homepage.kt
@@ -1,5 +1,6 @@
 package com.example.birdtrail_opsc7312
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.birdtrail_opsc7312.databinding.ActivityHomepageBinding
@@ -9,16 +10,41 @@ class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_homepage)
+        //setContentView(R.layout.activity_homepage)
 
         var binding = ActivityHomepageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-        //navigation bar
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.leaderboard -> {
+                    // Start Leaderboard Activity
+                    true
+                }
+                R.id.home -> {
+                    // Start Home Activity
+                    val intent = Intent(this, Homepage::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.observations -> {
+                    // Start Observations Activity
+                    true
+                }
+                R.id.settings -> {
+                    // Start Settings Activity
+                    val intent = Intent(this, SettingsPage::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
 
 
     }
