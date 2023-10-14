df91ff8fe7872364ef69477d9b303c6a9648c59b
CopperPaint <jsaul1250@gmail.com>
2023-10-14 17:38:34 +0200

    user observation data class

---
+++ app/src/main/java/com/example/birdtrail_opsc7312/UserObservationDataClass.kt
@@ -0,0 +1,16 @@
+package com.example.birdtrail_opsc7312
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+
+data class UserObservationDataClass (
+    var observationID: Int = 0,
+    var userID: Int = 0,
+    var lat: Double = 0.0,
+    var long: Double = 0.0,
+    var birdName: String = "",
+    var eBirdCode: String = "",
+    var date: LocalDate = LocalDate.now(),
+    var count: Int = 0
+)
