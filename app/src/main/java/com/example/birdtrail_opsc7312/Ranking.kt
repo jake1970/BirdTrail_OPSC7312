3c1c2e236c7268ceaed6d357ef0b774a70559edd
jake1970 <jakeyjyoung@gmail.com>
2023-10-15 21:23:39 +0200

    Added Achievement Styling

--- app/src/main/java/com/example/birdtrail_opsc7312/Ranking.kt
+++ app/src/main/java/com/example/birdtrail_opsc7312/Ranking.kt
@@ -1,10 +1,17 @@
 package com.example.birdtrail_opsc7312
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Insets
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsetsAnimation.Bounds
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
 import com.example.birdtrail_opsc7312.databinding.FragmentHomeBinding
 import com.example.birdtrail_opsc7312.databinding.FragmentRankingBinding
 
@@ -14,7 +21,8 @@ import com.example.birdtrail_opsc7312.databinding.FragmentRankingBinding
  * Use the [Ranking.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Ranking : Fragment() {
+@Suppress("DEPRECATION")
+class Ranking : Fragment(R.layout.fragment_ranking) {
 
     private var _binding: FragmentRankingBinding? = null
     private val binding get() = _binding!!
@@ -34,7 +42,7 @@ class Ranking : Fragment() {
         //Load initial leaderboard View
         populateViewContent(true)
 
-        binding.tvLeaderboardSwitch.setOnClickListener()
+        binding.tvLeaderboard.setOnClickListener()
         {
 
             //Animate the bar to the left side of the screen
@@ -44,7 +52,7 @@ class Ranking : Fragment() {
             populateViewContent(true)
         }
 
-        binding.tvLeaderboardSwitch.setOnClickListener()
+        binding.tvAchievements.setOnClickListener()
         {
             //Generate the position of the tab indicator bar at the end (right) of the screen
            val moveToPosition = animationManager.getPositionInRelationToScreen(binding.vwSelectedView)
@@ -63,6 +71,7 @@ class Ranking : Fragment() {
     //---------------------------------------------------------------------------------------------
     //Swap Ranking Views
     //---------------------------------------------------------------------------------------------
+    @SuppressLint("ResourceAsColor")
     private fun populateViewContent(leaderboardView: Boolean){
 
         //loop iterations
@@ -109,6 +118,28 @@ class Ranking : Fragment() {
 
                 //new dynamic component
                 var newAchievementCard = Card_Achievement(activity)
+
+
+                if (i == 1)
+                {
+                    //locked achievement
+                    newAchievementCard.binding.tvSelectorText.text = "99/100"
+                    newAchievementCard.binding.tvSelectorText.setCompoundDrawables(null,null,null,null);
+                    newAchievementCard.binding.tvSelectorText.setPadding(0,0,0,0)
+                    newAchievementCard.binding.rlSelector.background.setColorFilter(ContextCompat.getColor(requireContext(), R.color.dark_blue), android.graphics.PorterDuff.Mode.SRC_IN)
+
+                }
+
+                if (i == 2)
+                {
+                    //unlocked achievement achievement
+                    newAchievementCard.binding.tvSelectorText.text = ""
+                    newAchievementCard.binding.tvSelectorText.setPadding(0,72,0,0)
+                    newAchievementCard.binding.rlSelector.background.setColorFilter(ContextCompat.getColor(requireContext(), R.color.dark_blue), android.graphics.PorterDuff.Mode.SRC_IN)
+
+                }
+
+                //default is the select bird
 
                 //add the dynamic component to the container view
                 activityLayout.addView(newAchievementCard)
