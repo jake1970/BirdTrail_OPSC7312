aba0455a8a6191b30fbd847bfedf88802df076cc
jake1970 <jakeyjyoung@gmail.com>
2023-10-16 17:47:55 +0200

    Added Sign In and Sign Up

--- app/src/main/java/com/example/birdtrail_opsc7312/SignUp.kt
+++ app/src/main/java/com/example/birdtrail_opsc7312/SignUp.kt
@@ -1,5 +1,6 @@
 package com.example.birdtrail_opsc7312
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
@@ -67,8 +68,22 @@ class SignUp : AppCompatActivity() {
 
 
                 if (attemptRegister == "") {
-                    GlobalClass.InformUser(getString(R.string.completedSignUp), getString(R.string.instructToSignIn), this)
-                    finish()
+
+                   //GlobalClass.InformUser(getString(R.string.completedSignUp), getString(R.string.instructToSignIn), this)
+
+                    val alert = AlertDialog.Builder(this)
+                    alert.setTitle(getString(R.string.completedSignUp))
+                    alert.setMessage(getString(R.string.instructToSignIn))
+                    alert.setPositiveButton(getString(R.string.okText)){ dialog, which ->
+
+                        var intent = Intent(this, LandingPage::class.java)
+                        startActivity(intent)
+
+                    }
+                    alert.show()
+
+
+
                 } else {
                     GlobalClass.InformUser(getString(R.string.failedSignUp), attemptRegister, this)
                 }
