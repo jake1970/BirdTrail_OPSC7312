df8dae78238bbd106f3e2d2dca5f34cb2b365bf9
bbninja1 <shirk.benjamin@gmail.com>
2023-10-16 22:22:23 +0200

    Settings and fragment

--- app/src/main/java/com/example/birdtrail_opsc7312/AppSettings.kt
+++ app/src/main/java/com/example/birdtrail_opsc7312/AppSettings.kt
@@ -24,11 +24,6 @@ import java.io.IOException
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AppSettings.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AppSettings : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -123,14 +118,6 @@ class AppSettings : Fragment() {
     companion object {
 
         private const val REQUEST_PICK_IMAGE = 123
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AppSettings.
-         */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
