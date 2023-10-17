fe906b3cf0ddffe742da95fc6f255f4bb2a41e78
CopperPaint <jsaul1250@gmail.com>
2023-10-17 20:38:13 +0200

    home update

--- app/src/main/java/com/example/birdtrail_opsc7312/HomeFragment.kt
+++ app/src/main/java/com/example/birdtrail_opsc7312/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.example.birdtrail_opsc7312
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,10 +10,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.view.children
 import com.example.birdtrail_opsc7312.databinding.ActivityHomepageBinding
 import com.example.birdtrail_opsc7312.databinding.FragmentHomeBinding
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -56,18 +62,101 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         //88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
-
         val activityLayout = binding.llBirdSummaryInfo
 
-        val latestGeneralSighting = Card_Observations_Species(activity)
+        //======================================================================================================
+
+        //clostest sighting to user
+        val closestGeneralSighting = Card_Observations_Species(activity)
+
+        var pastDistance = 0.0
+        var pastHotspot  = eBirdJson2KtKotlin()
+
+        var userLocation: Location? = null
+        var mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        // Define the permission request
+        val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission has been granted, proceed with your operation
+                mFusedLocationClient.lastLocation.addOnCompleteListener(requireActivity()) { task ->
+                    userLocation = task.result
+                    if (userLocation != null) {
+                        GlobalScope.launch(Dispatchers.IO) {
+                            // Your code here...
+
+
+                            GlobalScope.launch (Dispatchers.IO) {
+                                // Iterate over each hotspot in the global list
+                                for (hotspot in GlobalClass.hotspots) {
+                                    // Calculate the distance between the user's location and the hotspot.
+                                    val distanceInKm = FullMapFragment().calculateDistance(userLocation!!.latitude, userLocation!!.longitude, hotspot.lat!!, hotspot.lng!!)
+                                    // If the distance is less than or equal to 50km, add an annotation for this hotspot.
+                                    if (distanceInKm <= 60 ) {
+                                        if (distanceInKm <= pastDistance || pastDistance == 0.0)
+                                        {
+                                            pastDistance = distanceInKm
+                                            pastHotspot = hotspot
+                                        }
+                                    }
+                                }
+                                withContext (Dispatchers.Main) {
+                                    closestGeneralSighting.binding.tvSpecies.text = pastHotspot.comName
+                                    closestGeneralSighting.binding.tvSighted.text = pastHotspot.obsDt
+                                    activityLayout.addView(closestGeneralSighting)
+
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                // Permission has been denied, handle accordingly
+            }
+        }
+
+        // Check if we have the permission
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // If not, request the permission
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        //======================================================================================================
+
+        //lastest user sighting
         val latestUserSighting = Card_Observations_Species(activity)
 
-        activityLayout.addView(latestGeneralSighting)
+        var observationlist = arrayListOf<UserObservationDataClass>()
+        //get latest sighting
+        for (i in 1..GlobalClass.userObservations.size)
+        {
+            if (GlobalClass.userObservations[i-1].userID == GlobalClass.currentUser.userID)
+            {
+                observationlist.add(GlobalClass.userObservations[i-1])
+            }
+        }
+        var userSighting = observationlist.last()
+
+        latestUserSighting.binding.tvSpecies.text = userSighting.birdName
+        latestUserSighting.binding.tvSighted.text = userSighting.date.toString()
+
+        //======================================================================================================
+        //add componets
+        //activityLayout.addView(closestGeneralSighting)
 
         //call method to generate a space under the dynamic component
         scrollViewTools.generateSpacer(activityLayout, requireActivity(), spacerSize)
 
-       activityLayout.addView(latestUserSighting)
+
+        activityLayout.addView(latestUserSighting)
+        //call method to generate a space under the dynamic component
+        scrollViewTools.generateSpacer(activityLayout, requireActivity(), spacerSize)
+        //======================================================================================================
 
 
 
