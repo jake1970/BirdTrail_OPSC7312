cdaf3f3fc23a11bd4496dce649b2cd1c87f74683
jake1970 <jakeyjyoung@gmail.com>
2023-11-09 22:36:44 +0200

    Fixed Back button on map directions

--- app/src/main/java/com/example/birdtrail_opsc7312/MapDirectionsActivity.kt
+++ app/src/main/java/com/example/birdtrail_opsc7312/MapDirectionsActivity.kt
@@ -469,11 +469,15 @@ class MapDirectionsActivity : AppCompatActivity()
             // initialize view interactions
             binding.btnBack.setOnClickListener {
 
+                finish()
+
+                /*
                 //back to home screen
                 val intent = Intent(this, Homepage::class.java).apply {
                     // Pass any data to Homepage Activity if needed
                 }
                 startActivity(intent)
+                 */
             }
 
             binding.recenter.setOnClickListener {
