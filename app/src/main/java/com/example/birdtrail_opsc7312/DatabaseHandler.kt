54c7000dc171103f9f1709d730a83756c0f8eb3d
CopperPaint <jsaul1250@gmail.com>
2023-11-11 15:31:26 +0200

    comment + try catch 3

--- app/src/main/java/com/example/birdtrail_opsc7312/DatabaseHandler.kt
+++ app/src/main/java/com/example/birdtrail_opsc7312/DatabaseHandler.kt
@@ -1,23 +1,18 @@
 package com.example.birdtrail_opsc7312
 
-import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
 import android.net.Uri
 import android.os.Build
-import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.graphics.drawable.toBitmap
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.tasks.await
 import java.io.File
-
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -27,6 +22,7 @@ class DatabaseHandler
     //database
     val db = Firebase.firestore
 
+    //method to get all data of all users form database
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun getAllUsers()
     {
@@ -44,6 +40,7 @@ class DatabaseHandler
             val score: Int = document.data.getValue("score").toString().toInt()
             val securityAnswer: String = document.data.getValue("securityAnswer").toString()
 
+            //create user object
             var user = UserDataClass(
                 userID = userID,
                 username = username,
@@ -56,20 +53,21 @@ class DatabaseHandler
                 securityanswer = securityAnswer
             )
 
-
-
+            //set current user
             if (userID == GlobalClass.currentUser.userID) {
                 if (GlobalClass.currentUser.profilepicture == null)
                 {
                     GlobalClass.currentUser = user
                 }
             }
-
-
+            //add user to user list
             GlobalClass.userData.add(user)
         }
     }
 
+    //----------------------------------------------------------------------------------------------
+
+    //method to update local data by fetching from database
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun updateLocalData()
     {
@@ -80,46 +78,59 @@ class DatabaseHandler
         GlobalClass.UpdateDataBase = false
     }
 
+    //----------------------------------------------------------------------------------------------
+
+    //method to get the current user's observation
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun getUserObservations()
     {
         GlobalClass.userObservations.clear()
 
         val querySnapshot = db.collection("UserObservations").get().await()
+        //data formatter
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         for (document in querySnapshot)
         {
-            val observationID: String = document.id
+            //if observation belongs to current user
             val userID: String = document.data.getValue("userID").toString()
-            val birdname: String = document.data.getValue("birdname").toString()
-            val count: Int = document.data.getValue("count").toString().toInt()
-            var datestring: String = document.data.getValue("date").toString()
-            var date = LocalDate.parse(datestring, formatter)
-            var lat: Double = document.data.getValue("lat").toString().toDouble()
-            var long: Double = document.data.getValue("long").toString().toDouble()
-            var time: String = document.data.getValue("time").toString()
+            if (userID==GlobalClass.currentUser.userID || userID!=null)
+            {
+                val observationID: String = document.id
+                val birdname: String = document.data.getValue("birdname").toString()
+                val count: Int = document.data.getValue("count").toString().toInt()
+                var datestring: String = document.data.getValue("date").toString()
+                var date = LocalDate.parse(datestring, formatter)
+                var lat: Double = document.data.getValue("lat").toString().toDouble()
+                var long: Double = document.data.getValue("long").toString().toDouble()
+                var time: String = document.data.getValue("time").toString()
 
-            var observation = UserObservationDataClass(
-                observationID = observationID,
-                userID = userID,
-                lat = lat,
-                long = long,
-                birdName = birdname,
-                count = count,
-                date = date,
-                time = time
-            )
-            GlobalClass.userObservations.add(observation)
+                //create observation
+                var observation = UserObservationDataClass(
+                    observationID = observationID,
+                    userID = userID,
+                    lat = lat,
+                    long = long,
+                    birdName = birdname,
+                    count = count,
+                    date = date,
+                    time = time
+                )
+                //add observation to observation list
+                GlobalClass.userObservations.add(observation)
+            }
         }
     }
 
+    //----------------------------------------------------------------------------------------------
+
+    //method to get the achievements of all users
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun getUserAchievements()
     {
-
         GlobalClass.userAchievements.clear()
 
         val querySnapshot = db.collection("UserAchievements").get().await()
+        //data formatter
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         for (document in querySnapshot) {
             val achID = document.data.getValue("achID").toString().toInt()
@@ -127,6 +138,7 @@ class DatabaseHandler
             var date = LocalDate.parse(datestring, formatter)
             var userID = document.data.getValue("userID").toString()
 
+            //create achievement
             var userAchievement = UserAchievementsDataClass(
                 userID = userID,
                 achID = achID,
@@ -136,6 +148,9 @@ class DatabaseHandler
         }
     }
 
+    //----------------------------------------------------------------------------------------------
+
+    //method to get security questions
     suspend fun getQuestions()
     {
         GlobalClass.questions.clear()
@@ -145,19 +160,22 @@ class DatabaseHandler
             val questionID: String = document.id
             val question: String = document.data.getValue("question").toString()
 
+            //create question
             var newQuestion =  QuestionsDataClass(
                 questionID = questionID,
                 question = question
             )
+            //add question to list
             GlobalClass.questions.add(newQuestion)
         }
     }
 
+    //----------------------------------------------------------------------------------------------
 
+    //method to add user to database
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun AddUser(newUser: UserDataClass, userID: String)
     {
-
         db.collection("Users")
             .document(userID)
             .set(mapOf(
@@ -171,39 +189,30 @@ class DatabaseHandler
                 "securityAnswer" to newUser.securityanswer
             )).addOnSuccessListener { GlobalClass.UpdateDataBase = true }
             .await()
-
-        //getAllUsers()
-
-//        db.collection("Users")
-//            .add(mapOf(
-//                "username" to newUser.username,
-//                "badgeID" to newUser.badgeID,
-//                "defaultDistance" to newUser.defaultdistance,
-//                "hasProfile" to newUser.hasProfile,
-//                "isMetric" to newUser.isMetric,
-//                "questionID" to newUser.questionID,
-//                "score" to newUser.score,
-//                "securityAnswer" to newUser.securityanswer
-//            )).await()
     }
 
+    //----------------------------------------------------------------------------------------------
+
+    //method to add user observation to database
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun AddUserObservation(newObservation: UserObservationDataClass)
     {
         db.collection("UserObservations")
             .add(mapOf(
                 "userID" to newObservation.userID,
-                "date" to LocalDate.now().toString(),
-                "time" to getCurrentTime(),
+                "date" to LocalDate.now().toString(), //use current date
+                "time" to getCurrentTime(), //use current time
                 "birdname" to newObservation.birdName,
                 "lat" to newObservation.lat,
                 "long" to newObservation.long,
                 "count" to newObservation.count
             )).addOnSuccessListener { GlobalClass.UpdateDataBase = true }
             .await()
-        //getUserObservations()
     }
 
+    //----------------------------------------------------------------------------------------------
+
+    //method to add user achievement to database
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun AddUserAchievements(newUserAchievement: UserAchievementsDataClass)
     {
@@ -214,10 +223,11 @@ class DatabaseHandler
                 "date" to newUserAchievement.date.toString()
             )).addOnSuccessListener { GlobalClass.UpdateDataBase = true }
             .await()
-        //getUserAchievements()
     }
 
+    //----------------------------------------------------------------------------------------------
 
+    //method to update user in database
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun updateUser(updatedUser: UserDataClass)
     {
@@ -230,11 +240,11 @@ class DatabaseHandler
                 "score" to updatedUser.score
             )
         ).addOnSuccessListener { GlobalClass.UpdateDataBase = true }.await()
-        //getAllUsers()
     }
 
+    //----------------------------------------------------------------------------------------------
 
-
+    //method to get the current time as a string
     @RequiresApi(Build.VERSION_CODES.O)
     fun getCurrentTime(): String {
         val current = LocalDateTime.now()
@@ -242,15 +252,15 @@ class DatabaseHandler
         return current.format(formatter)
     }
 
+    //----------------------------------------------------------------------------------------------
 
-
+    //method to get the user's profile image
     suspend fun getUserImage(context: Context, userID: String, userHasImage: Boolean): Bitmap? {
 
         var defaultUserImage = context.getDrawable(R.drawable.imgdefaultprofile)?.toBitmap()
 
         if (userHasImage == true) {
             try {
-
                 val storageReference = FirebaseStorage.getInstance().reference.child("UserImages/$userID")
 
                 val imgFile = File.createTempFile("tempImage", "jpg")
@@ -263,11 +273,12 @@ class DatabaseHandler
                 defaultUserImage = context.getDrawable(R.drawable.imgdefaultprofile)?.toBitmap()
             }
         }
-
         return defaultUserImage
     }
 
+    //----------------------------------------------------------------------------------------------
 
+    //method to set user image
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun setUserImage(context: Context, userID: String, selectedImageUri: Uri) {
 
@@ -282,7 +293,5 @@ class DatabaseHandler
                 GlobalClass.currentUser.hasProfile = true
                 Toast.makeText(context, "Updated Profile Image", Toast.LENGTH_SHORT).show()
             }.await()
-
     }
-
 }
